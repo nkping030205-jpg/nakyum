f87849f961d365247eb2d5236889d66e50801ae9
nkping030205-jpg <nkping030205@gmail.com>
2025-09-18 16:42:24 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+A = input().split()
+B = ['1', '2', '3', '4', '5', '6', '7', '8']
+C = ['8', '7', '6', '5', '4', '3', '2', '1']
+if A == B:
+    print("ascending")
+elif A == C:
+    print("descending")
+else:
+    print("mixed")
